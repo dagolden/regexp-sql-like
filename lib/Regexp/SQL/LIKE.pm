8d3e4ca3fd603f655f4ba90a26524b6a5e73a2d9
David Golden <dagolden@cpan.org>
2011-07-05 17:30:50 -0400

    initial import of Regexp-SQL-LIKE

---
+++ lib/Regexp/SQL/LIKE.pm
@@ -0,0 +1,40 @@
+use 5.008001;
+use strict;
+use warnings;
+use utf8;
+
+package Regexp::SQL::LIKE;
+# ABSTRACT: No abstract given for Regexp::SQL::LIKE
+
+# Dependencies
+use autodie 2.00;
+
+1;
+
+__END__
+
+=for Pod::Coverage method_names_here
+
+=begin wikidoc
+
+= SYNOPSIS
+
+  use Regexp::SQL::LIKE;
+
+= DESCRIPTION
+
+This module might be cool, but you'd never know it from the lack
+of documentation.
+
+= USAGE
+
+Good luck!
+
+= SEE ALSO
+
+Maybe other modules do related things.
+
+=end wikidoc
+
+=cut
+
